83edf3a04bf94a3faeb94b95ef21972f3cf7fe6b
Kurtuluş Ruşen Öztürk <92689191+Kurtulusozturk@users.noreply.github.com>
2023-08-10 00:06:59 +0300

    fix

--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var redirectButtonCa = document.getElementById('redirectButtonCa');
     var redirectButtonSa = document.getElementById('redirectButtonSa');
+    var redirectButtonCaSa = document.getElementById('redirectButtonCaSa');
     var redirectButtonCom = document.getElementById('redirectButtonCom');
     var currentURL = "";
+
     var check = 0;
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         var currentTab = tabs[0];
@@ -13,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         if(currentURL.includes("https://www.amazon.com")){
             redirectButtonCom.disabled = true;
+            redirectButtonCaSa.disabled = true;
         }
       });
 
@@ -26,17 +29,25 @@ document.addEventListener('DOMContentLoaded', function() {
         Redirect('https://www.amazon.com','https://www.amazon.sa',check)
     });
     
+    redirectButtonCaSa.addEventListener('click', function() {
+        check = 0;
+        if(currentURL.includes("https://www.amazon.ca")){
+            Redirect('https://www.amazon.ca','https://www.amazon.sa',check)
+        }else if(currentURL.includes(("https://www.amazon.sa"))){
+            Redirect('https://www.amazon.sa','https://www.amazon.ca',check)
+        }
+    });
+
     redirectButtonCom.addEventListener('click', function() {
         check = 1;
         Redirect('https://www.amazon.ca','https://www.amazon.sa',check)
     });
+
     function Redirect(url1, url2 , check) {
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             var tab = tabs[0];
             if(check == 0){
-                if(currentURL.includes("https://www.amazon.com")){
-                    var newURL = tab.url.replace(url1, url2);
-                }
+                var newURL = tab.url.replace(url1, url2);
             }else{
                 if(currentURL.includes(url1)){
                     var newURL = tab.url.replace(url1, 'https://www.amazon.com');
